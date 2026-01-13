90628d462adf2684992a05e9c826d7e83545d016
Joao de Deus <joaodeus400@gmail.com>
2026-01-13 14:43:53 -0300

    refactor: controller cults and music

--- EscalaSistema.API/Controllers/CultsController.cs
+++ EscalaSistema.API/Controllers/CultsController.cs
@@ -1,47 +1,36 @@
-﻿using EscalaSistema.API.Data;
-using EscalaSistema.API.DTOs;
-using EscalaSistema.API.Models;
-using EscalaSistema.API.Validation;
-using FluentValidation;
+﻿using EscalaSistema.API.DTOs;
+using EscalaSistema.API.Interface.UseCase;
 using Microsoft.AspNetCore.Mvc;
+
+namespace EscalaSistema.API.Controllers;
 
 [ApiController]
 [Route("api/cults")]
 public class CultController : ControllerBase
 {
-    private readonly EscalaSistemaDbContext _db;
-
-    public CultController(EscalaSistemaDbContext db)
-    {
-        _db = db;
-    }
-
     [HttpPost]
-    public async Task<IActionResult> Create([FromBody] CultResponse cut, [FromServices] IValidator<Cult> validator)
+    [Route("create")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> Create([FromBody] CultResponse cut, [FromServices] ICultUseCase cultUseCase)
     {
-        var cult = new Cult
-        {
-            Id = Guid.NewGuid(),
-            Name = cut.Name,
-            DateTime = cut.Date
-        };
-        var validationResult = await validator.ValidateAsync(cult);
-
-        if (!validationResult.IsValid)
-        {
-            // Retorna HTTP 400 com a lista de erros
-            return BadRequest(validationResult.Errors);
-        }
-        _db.Cults.Add(cult);
-        await _db.SaveChangesAsync();
+        var cult = await cultUseCase.Register(cut);
 
         return Ok(cult);
     }
 
     [HttpGet]
-    public IActionResult Get()
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> Get([FromServices] ICultUseCase cultUseCase)
     {
-        return Ok(_db.Cults.ToList());
+        var cults = await cultUseCase.GetAllCultsAsync();
+
+        return Ok(cults);
     }
 }
 
---
+++ EscalaSistema.API/Controllers/MusicsController.cs
@@ -0,0 +1,18 @@
+﻿using EscalaSistema.API.DTOs;
+using EscalaSistema.API.Interface.UseCase;
+using EscalaSistema.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EscalaSistema.API.Controllers;
+
+[ApiController]
+[Route("api/musics")]
+public class MusicsController: ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] CreateMusicRequest musicResponse, [FromServices] IMusicUseCase musicUseCase)
+    {
+        var music = await musicUseCase.Register(musicResponse);
+        return Ok(music);
+    }
+}
