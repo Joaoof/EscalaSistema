0a0f31c6090a24301608e5e2bcae1012d79dfc4a
Joao de Deus <joaodeus400@gmail.com>
2026-01-13 09:23:48 -0300

    feat: add fluentValidation

---
+++ EscalaSistema.API/Validation/CultValidation.cs
@@ -0,0 +1,16 @@
+﻿using EscalaSistema.API.Models;
+using FluentValidation;
+
+namespace EscalaSistema.API.Validation
+{
+    public class CultValidation: AbstractValidator<Cult>
+    {
+        public CultValidation()
+        {
+            RuleFor(cult => cult.Name)
+                .NotEmpty().WithMessage("O nome do culto é obrigatório.")
+                .MaximumLength(20).WithMessage("O nome do culto não pode exceder 20 caracteres.");
+            RuleFor(cult => cult.DateTime).NotEmpty().WithMessage("A data e hora do culto são obrigatórias.");
+        }
+    }
+}
