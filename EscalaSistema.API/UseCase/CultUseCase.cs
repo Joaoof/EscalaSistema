7781100a32d3750bc577ddc112943778860f6cf8
Joao de Deus <joaodeus400@gmail.com>
2026-01-13 14:43:00 -0300

    feat: add useCase music

--- EscalaSistema.API/UseCase/CultUseCase.cs
+++ EscalaSistema.API/UseCase/CultUseCase.cs
@@ -1,6 +1,43 @@
-﻿namespace EscalaSistema.API.UseCase;
+﻿using EscalaSistema.API.DTOs;
+using EscalaSistema.API.Interface.Repository;
+using EscalaSistema.API.Interface.UseCase;
+using EscalaSistema.API.Models;
+using FluentValidation;
 
-public class CultUseCase
+namespace EscalaSistema.API.UseCase;
+
+public class CultUseCase : ICultUseCase
 {
+    private readonly ICultRepository _cultRepository;
+    private readonly IValidator<Cult> _validator;
+    public CultUseCase(ICultRepository cultRepository, IValidator<Cult> validator)
+    {
+        _cultRepository = cultRepository;
+        _validator = validator;
+    }
 
+    public async Task<Cult> Register(CultResponse cultResponse)
+    {
+        var cult = new Cult
+        {
+            Id = Guid.NewGuid(),
+            Name = cultResponse.Name,
+            DateTime = cultResponse.Date,
+        };
+
+        var validationResult = await _validator.ValidateAsync(cult);
+        if (!validationResult.IsValid)
+        {
+            throw new ValidationException(validationResult.Errors);
+        }
+
+        await _cultRepository.AddAsync(cult);
+        return cult;
+
+    }
+
+    public async Task<IEnumerable<Cult>> GetAllCultsAsync()
+    {
+        return await _cultRepository.GetAllAsync();
+    }
 }
---
+++ EscalaSistema.API/UseCase/MusicUseCase.cs
@@ -0,0 +1,46 @@
+﻿using EscalaSistema.API.DTOs;
+using EscalaSistema.API.Interface.Repository;
+using EscalaSistema.API.Interface.UseCase;
+using EscalaSistema.API.Models;
+using FluentValidation;
+
+namespace EscalaSistema.API.UseCase;
+
+public class MusicUseCase: IMusicUseCase
+{
+    private readonly IMusicRepository _musicRepository;
+    private readonly IValidator<Music> _validator;
+
+    public MusicUseCase(IMusicRepository musicRepository, IValidator<Music> validator)
+    {
+        _musicRepository = musicRepository;
+        _validator = validator;
+    }
+    public async Task<MusicResponse> Register(CreateMusicRequest music)
+    {
+        var musicModel = new Music
+        {
+            Name = music.Name,
+            Key = music.Key,
+            YoutubeUrl = music.YoutubeUrl,
+            CultId = music.CultId
+        };
+
+        var validationResult = await _validator.ValidateAsync(musicModel);
+        if (!validationResult.IsValid)
+        {
+            throw new ValidationException(validationResult.Errors);
+        }
+        await _musicRepository.AddAsync(musicModel);
+
+        var response = new MusicResponse
+        {
+            Name = musicModel.Name,
+            Key = musicModel.Key,
+            YoutubeUrl = musicModel.YoutubeUrl,
+            CultId = musicModel.CultId
+        };
+        return response;
+    }
+
+}
