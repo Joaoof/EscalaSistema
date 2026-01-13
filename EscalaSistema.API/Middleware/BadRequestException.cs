96aa691149126172171f5445cdd753c4e2d95144
Joao de Deus <joaodeus400@gmail.com>
2026-01-13 14:43:25 -0300

    feat: add exception middleware

---
+++ EscalaSistema.API/Middleware/BadRequestException.cs
@@ -0,0 +1,11 @@
+﻿namespace EscalaSistema.API.Middleware;
+
+public class BadRequestException: Exception
+{
+    public int StatusCode { get; }
+    public BadRequestException(string message, int statusCode) : base(message) 
+    {
+        StatusCode = 400;
+    }
+
+}
---
+++ EscalaSistema.API/Middleware/CustomExceptionMiddleware.cs
@@ -0,0 +1,49 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace EscalaSistema.API.Middleware;
+
+public class CustomExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<CustomExceptionMiddleware> _logger;
+
+    public CustomExceptionMiddleware(RequestDelegate next, ILogger<CustomExceptionMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+           await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        _logger.LogError(exception, exception.Message);
+
+        int statusCode = exception switch
+        {
+            BadRequestException br => br.StatusCode,
+            _ => StatusCodes.Status500InternalServerError
+        };
+
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = statusCode;
+
+        await context.Response.WriteAsync(JsonSerializer.Serialize(new
+        {
+            success = false,
+            message = exception.Message
+        }));
+    }
+
+}
