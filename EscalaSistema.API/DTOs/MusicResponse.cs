5918786e8e10099dfe2247fceb25b84a1015e31a
Joao de Deus <joaodeus400@gmail.com>
2026-01-13 14:44:08 -0300

    feat: add dDtos response and request

--- EscalaSistema.API/DTOs/MusicResponse.cs
+++ EscalaSistema.API/DTOs/MusicResponse.cs
@@ -1,9 +1,11 @@
 ﻿namespace EscalaSistema.API.DTOs;
 
-public class CreateMusicRequest
+public class MusicResponse
 {
+    public Guid Id { get; set; }
     public string Name { get; set; }
     public string Key { get; set; }
     public string YoutubeUrl { get; set; }
+
     public Guid CultId { get; set; }
 }
