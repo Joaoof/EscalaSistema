eea7bbc599b64b8203e039c70bee47a2aea3ea5e
Joao de Deus <joaodeus400@gmail.com>
2026-01-13 14:44:18 -0300

    feat: add validation music fluent

---
+++ EscalaSistema.API/Validation/MusicValidation.cs
@@ -0,0 +1,18 @@
+﻿using EscalaSistema.API.Models;
+using FluentValidation;
+
+namespace EscalaSistema.API.Validation;
+
+public class MusicValidation: AbstractValidator<Music>
+{
+    public MusicValidation()
+    {
+        RuleFor(music => music.Name)
+            .NotEmpty().WithMessage("O título da música é obrigatório.")
+            .MaximumLength(200).WithMessage("O título da música não pode exceder 200 caracteres.");
+        RuleFor(music => music.Key).NotEmpty().WithErrorCode("A tonalidade da música é obrigatória.").WithMessage("A tonalidade da música é obrigatória.");
+        RuleFor(music => music.YoutubeUrl)
+            .NotEmpty().WithMessage("A URL do YouTube é obrigatória.")
+            .MaximumLength(200).WithMessage("A URL do YouTube não pode exceder 200 caracteres.");
+    }
+}
