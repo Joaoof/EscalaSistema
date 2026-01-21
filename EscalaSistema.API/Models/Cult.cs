da37ae8e39cb5f9d06f2ed97092564798c71a982
Joao de Deus <joaodeus400@gmail.com>
2026-01-21 17:01:24 -0300

    feat: rich modeling

--- EscalaSistema.API/Models/Cult.cs
+++ EscalaSistema.API/Models/Cult.cs
@@ -1,14 +1,34 @@
-﻿namespace EscalaSistema.API.Models;
+﻿using EscalaSistema.API.Domain.Errors;
+
+namespace EscalaSistema.API.Models;
 
 public class Cult
 {
-    public Guid Id { get; set; }
-    public required string Name { get; set; }
-    public DateTime DateTime { get; set; }
-    public DateTime IsPublished { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public DateTime UpdatedAt { get; set; }
-    public Guid MusicId { get; set; }
-    public ICollection<Music> Musics { get; set; }
-    public Scale Scale { get; set; }
+    public Guid Id { get; private set; }
+    public string Name { get; private set; }
+    public DateTime DateTime { get; private set; }
+    public bool IsPublished { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    public DateTime UpdatedAt { get; private set; }
+    public Guid MusicId { get; private set; }
+    public ICollection<Music> Musics { get; private set; }
+    public Scale Scale { get; private set; }
+
+    public Cult(string name, DateTime dateTime)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new DomainException(CultErrors.InvalidNameCult);
+
+        if (dateTime < DateTime.Now)
+            throw new DomainException(CultErrors.InvalidDateTimeCannotFuture);
+
+        Name = name;
+        DateTime = dateTime;
+        IsPublished = false; 
+    }
+
+    public void Publicar()
+    {
+        IsPublished = true;
+    }
 }
